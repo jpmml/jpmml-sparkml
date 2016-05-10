2cf9bbf3cc11db3a2df8504926dd43cf03cc9e31
Villu Ruusmann <villu.ruusmann@gmail.com>
2016-05-11 00:38:41 +0300

    Applied an ad hoc correction to the threshold value of continuous splits

--- src/main/java/org/jpmml/sparkml/TreeModelUtil.java
+++ src/main/java/org/jpmml/sparkml/TreeModelUtil.java
@@ -188,7 +188,12 @@ public class TreeModelUtil {
 	private Predicate[] encodeContinuousSplit(ContinuousSplit continuousSplit, FeatureSchema schema){
 		ContinuousFeature feature = (ContinuousFeature)schema.getFeature(continuousSplit.featureIndex());
 
-		String value = ValueUtil.formatValue(continuousSplit.threshold());
+		double threshold = continuousSplit.threshold();
+
+		// XXX
+		threshold += 1e-15;
+
+		String value = ValueUtil.formatValue(threshold);
 
 		SimplePredicate leftPredicate = new SimplePredicate(feature.getName(), SimplePredicate.Operator.LESS_OR_EQUAL)
 			.setValue(value);
