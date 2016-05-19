8b804ae55bb372a1b14b8514f65a295868932506
Villu Ruusmann <villu.ruusmann@gmail.com>
2016-05-19 11:46:14 +0300

    Tightened the default batch testing acceptance criteria thousandfold

--- src/test/java/org/jpmml/sparkml/ConverterTest.java
+++ src/test/java/org/jpmml/sparkml/ConverterTest.java
@@ -2,11 +2,14 @@ package org.jpmml.sparkml;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Set;
 
 import org.apache.spark.ml.PipelineModel;
 import org.apache.spark.sql.types.StructType;
+import org.dmg.pmml.FieldName;
 import org.dmg.pmml.PMML;
 import org.jpmml.evaluator.ArchiveBatch;
+import org.jpmml.evaluator.Batch;
 import org.jpmml.evaluator.IntegrationTest;
 import org.jpmml.model.SerializationUtil;
 
@@ -42,5 +45,10 @@ public class ConverterTest extends IntegrationTest {
 		};
 
 		return result;
+	}
+
+	@Override
+	public void evaluate(Batch batch, Set<FieldName> ignoredFields) throws Exception {
+		super.evaluate(batch, ignoredFields, 1e-12, 1e-12);
 	}
 }
