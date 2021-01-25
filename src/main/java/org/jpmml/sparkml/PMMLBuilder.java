33e650bc23a5c1b506186ee32cadd4f1b5c9ae66
Villu Ruusmann <villu.ruusmann@gmail.com>
2021-01-25 13:28:35 +0200

    Refactored the encoding of PMML elements. Fixes #103

--- src/main/java/org/jpmml/sparkml/PMMLBuilder.java
+++ src/main/java/org/jpmml/sparkml/PMMLBuilder.java
@@ -153,19 +153,19 @@ public class PMMLBuilder {
 
 		org.dmg.pmml.Model model;
 
+		if(models.size() == 0){
+			model = null;
+		} else
+
 		if(models.size() == 1){
 			model = Iterables.getOnlyElement(models);
 		} else
 
-		if(models.size() > 1){
-			model = MiningModelUtil.createModelChain(models);
-		} else
-
 		{
-			throw new IllegalArgumentException("Expected a pipeline with one or more models, got a pipeline with zero models");
+			model = MiningModelUtil.createModelChain(models);
 		} // End if
 
-		if(postProcessorNames.size() > 0){
+		if((model != null) && (postProcessorNames.size() > 0)){
 			org.dmg.pmml.Model finalModel = MiningModelUtil.getFinalModel(model);
 
 			Output output = ModelUtil.ensureOutput(finalModel);
@@ -185,7 +185,7 @@ public class PMMLBuilder {
 
 		PMML pmml = encoder.encodePMML(model);
 
-		if((predictionColumns.size() > 0 || probabilityColumns.size() > 0) && (verification != null)){
+		if((model != null) && (predictionColumns.size() > 0 || probabilityColumns.size() > 0) && (verification != null)){
 			Dataset<Row> dataset = verification.getDataset();
 			Dataset<Row> transformedDataset = verification.getTransformedDataset();
 			Double precision = verification.getPrecision();
